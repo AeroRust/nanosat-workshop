ea683df5dc2a478325f00302cfd29db98a4c0d35
Lachezar Lechev <elpiel93@gmail.com>
2024-12-31 14:50:20 +0200

    fix(obc): pico-w - main binary for RP23xx
    Signed-off-by: Lachezar Lechev <elpiel93@gmail.com>

--- onboard-computer/pico-w/src/main.rs
+++ onboard-computer/pico-w/src/main.rs
@@ -9,14 +9,31 @@ use cortex_m_rt::{exception, ExceptionFrame};
 #[cfg(feature = "cortex-m")]
 use panic_probe as _;
 
-#[cfg(feature = "rp2040")]
+#[cfg_attr(feature = "rp23", link_section = ".start_block")]
+#[cfg_attr(feature = "rp23", used)]
+pub static IMAGE_DEF: embassy_rp::block::ImageDef = embassy_rp::block::ImageDef::secure_exe();
+
+// Program metadata for `picotool info`.
+// This isn't needed, but it's recomended to have these minimal entries.
+#[cfg_attr(feature = "rp23", link_section = ".bi_entries")]
+#[cfg_attr(feature = "rp23", used)]
+pub static PICOTOOL_ENTRIES: [embassy_rp::binary_info::EntryAddr; 4] = [
+    embassy_rp::binary_info::rp_program_name!(c"Blinky Example"),
+    embassy_rp::binary_info::rp_program_description!(
+        c"This example tests the RP Pico on board LED, connected to gpio 25"
+    ),
+    embassy_rp::binary_info::rp_cargo_version!(),
+    embassy_rp::binary_info::rp_program_build_attribute!(),
+];
+
+#[cfg(any(feature = "rp2040", feature = "rp23"))]
 #[cortex_m_rt::entry]
 fn main() -> ! {
     let application = pico_w::Application::init();
     application.run()
 }
 
-#[cfg(feature = "rp2040")]
+#[cfg(any(feature = "rp2040", feature = "rp23"))]
 #[exception]
 unsafe fn HardFault(ef: &ExceptionFrame) -> ! {
     use defmt::error;
